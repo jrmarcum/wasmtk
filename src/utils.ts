6245e4ce4e11e8079695398da64c6680b1e00908
Jon Marcum <jrmarcum.se@gmail.com>
2026-06-16 18:20:27 -0400

    bump version to v1.8.0

--- src/utils.ts
+++ src/utils.ts
@@ -33,7 +33,7 @@ interface WabtModule {
 }
 
 /** The current version of the wasmtk toolkit. */
-export const VERSION = "1.7.0";
+export const VERSION = "1.8.0";
 
 let wasiInstance: WebAssembly.Instance | undefined;
 
